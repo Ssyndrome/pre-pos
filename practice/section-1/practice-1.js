c38a020d809ef658855ff6f4ba0e4e28defa5247
915448715@qq.com <915448715@qq.com>
2017-07-22 12:38:43 +0800

    successfully create p1.1 p1.2 p1.3

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,15 +1,15 @@
 ﻿'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-	collectionA = collectionA.sort();
-	collectionB = collectionB.sort();
-	var commn = [];
-	for (i = 0;i<collectionA.length;i++){
-		for(j = 0;j<collectionB.length;j++ ){
-			if collectionA[i] = collectionB[j]
+	
+	var commn = new Array;
+	for (var i = 0;i < collectionA.length;i++){
+		for(var j = 0;j < collectionB.length;j++ ){
+			if (collectionA[i] == collectionB[j]){
 			commn.push(collectionA[i]);
+			}
 		}
 	}
-	commn = commn.sort;
+	
   return commn;
 }
