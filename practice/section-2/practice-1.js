fe69936f4366e349c71c526a04895c3da5fc08cd
915448715@qq.com <915448715@qq.com>
2017-07-24 10:15:01 +0800

    create p2.1

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,31 @@
 'use strict';
 
-function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
-}
+function countSameElements(collection){
+		
+		//算出输入数组有多少个不同值
+		var res = [collection[0]];
+		for(var x = 1; x < collection.length; x++){
+			if(collection[x] !== res[res.length - 1]){
+			res.push(collection[x]);
+			}
+        }
+		
+		//定义输出为二维数组
+		var outList = new Array; 
+		for(var q = 0;q < res.length;q++){
+			outList[q] = {};
+		}
+		console.log(res.length);
+		//循环检索重复元素
+		for(var i = 0;i < res.length;i++){
+			var y = 1;
+			for(var z = 0;collection[z] == collection[z+1];z++){
+				y++;
+				outList[i].key = collection[z];
+				outList[i].count = y;
+			}
+			collection.splice(0,z+1);
+		}
+		
+		return outList;
+	}
